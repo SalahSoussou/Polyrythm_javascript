3a1e38eff1630cf88012dd05544c932247dc3e12
SalahSoussou <soussousalahiddine@gmail.com>
2024-08-18 20:21:31 +0100

    ok

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ myCanvas.height = size;
 
 const trackCenter = { x: size / 2, y: size / 2 };
 const trackRadius = 100;
+const ballRadius = 10;
+const ballSpeed = 0.1;
 
 const ctx = myCanvas.getContext("2d");
 class Track {
@@ -12,11 +14,47 @@ class Track {
     this.center = center;
     this.radius = radius;
   }
+  getPosition(offset) {
+    return {
+      x: this.center.x + Math.cos(offset) * this.radius,
+      y: this.center.y - Math.sin(offset) * this.radius,
+    };
+  }
   draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
     ctx.stroke();
   }
 }
+
+class Ball {
+  constructor(track, radius, speed) {
+    this.track = track;
+    this.speed = speed;
+    this.radius = radius;
+    this.offset = 0;
+    this.center = this.track.getPosition(this.offset);
+  }
+  move() {
+    this.offset += this.speed;
+    this.center = this.track.getPosition(this.offset);
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
+
 const track = new Track(trackCenter, trackRadius);
-track.draw(ctx);
+const ball = new Ball(track, ballRadius, ballSpeed);
+
+animate();
+
+function animate() {
+  ctx.clearRect(0, 0, size, size);
+  track.draw(ctx);
+  ball.move();
+  ball.draw(ctx);
+  requestAnimationFrame(animate);
+}
